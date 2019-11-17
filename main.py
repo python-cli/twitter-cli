f2f0d7818f1428517df951f05b5080cc8e2a92d5
Wei Han <xingheng907@hotmail.com>
2019-11-17 22:47:41 +0800

    Fetch the all the favorite statuses by page.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from logger import getLogger
 import twitter
 from os.path import split
+from time import sleep
 
 from config import *
 from models import *
@@ -30,8 +31,24 @@ def print_sample_entity(prefix, entities):
 
     logger.info(prefix + content)
 
-def paginate_favorites():
-    pass
+def destroy_favorited_status(status):
+    if not status or not status.id:
+        return False
+
+    try:
+        logger.info('Destroying the favorited status %s' % status.id)
+        result = api.DestroyFavorite(status=status)
+        logger.info('Destroy finished!')
+        return result != None
+    except twitter.TwitterError as e:
+        logger.exception(e)
+
+        if e.args[0][0]['code'] == 144:
+            # TwitterError: No status found with that ID
+            # Let's just pretend it as suceed.
+            return True
+
+    return False
 
 def save_statuses(statuses):
     for status in statuses:
@@ -49,6 +66,9 @@ def save_statuses(statuses):
             if filepath:
                 video.downloaded_path = filepath
                 video.save()
+
+                if destroy_favorited_status(status):
+                    continue
             else:
                 logger.error('Download video failed?')
 
@@ -60,12 +80,54 @@ def save_statuses(statuses):
                 if filepath:
                     p.downloaded_path = filepath
                     p.save()
+
+                    if destroy_favorited_status(status):
+                        continue
                 else:
                     logger.error('Download photo failed?')
+
+def fetch_favorites(max_id=None):
+    statuses = None
+
+    try:
+        logger.info('Request start with: %s' % (str(max_id) or 'latest'))
+        statuses = api.GetFavorites(count=100, max_id=max_id)
+        logger.info('Request completed with %d status(es)', len(statuses) if statuses else 0)
+    except twitter.TwitterError as e:
+        logger.exception(e)
+
+        if e.args[0][0]['code'] == 88:
+            # TwitterError: Rate limit exceeded.
+            # https://developer.twitter.com/en/docs/basics/rate-limiting
+
+            # Let's try it in next hour later.
+            logger.info('Start sleeping')
+            sleep(24 * 60 * 60)
+            logger.info('End sleeping')
+
+            fetch_favorites(max_id)
+    except Exception as e:
+        logger.exception(e)
+
+    if statuses and len(statuses) > 0:
+        save_statuses(statuses)
+        logger.info('Save completed!')
+
+        last_id = statuses[-1].id
+
+        if last_id == max_id:
+            logger.info('This should be the end.')
+            return
+
+        fetch_favorites(last_id)
 
 
 userinfo = api.VerifyCredentials()
 logger.info('Verification pass!' if userinfo else 'Verification Failed')
+
+# status = Status.select().order_by(Status.id).first()
+# fetch_favorites(max_id=status.id if status else None)
+fetch_favorites()
 
 # statuses = api.GetUserTimeline(userinfo.id)
 # print_sample_entity('User status: ', statuses)
@@ -73,9 +135,3 @@ logger.info('Verification pass!' if userinfo else 'Verification Failed')
 # statuses = api.GetHomeTimeline()
 # print_sample_entity('Home status: ', statuses)
 
-statuses = api.GetFavorites(count=100)
-# print_sample_entity('Favorite status: ', statuses)
-logger.info('Request completed!')
-
-save_statuses(statuses)
-logger.info('Save completed!')
